8a8143d769c7dc4f69c573c1e8a599c29fe6759d
cdillond <93200981+cdillond@users.noreply.github.com>
2024-08-01 13:29:32 -0400

    added fastRand func

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/ecdsa"
+	"crypto/rand"
 	"errors"
 	"flag"
 	"fmt"
@@ -14,13 +15,46 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
+type keyFunc func() (*ecdsa.PrivateKey, error)
+
+// vars used by fastRand
+var (
+	rbuf []byte
+	bufN int
+)
+
+const RBUF_LEN = 4096
+
+// fastRand reads random data into rbuf and then converts slices of this data into private keys.
+// the beginning/end indices of the private key slice are incremented by 1 with each call, so the
+// underlying bytes are reused (until rbuf is exhausted and refilled), but they are interpreted
+// differently by crypto.ToECDSA. In theory, this should greatly reduce the number of syscalls
+// and copies for most prefixes. This would be bad if we were producing multiple private keys,
+// since it could potentially be much easier to guess private keys produced by overlapping data,
+// but, because we are only after 1 key, it is probably fine.
+func fastRand() (pk *ecdsa.PrivateKey, err error) {
+	if bufN == 0 || bufN > RBUF_LEN-32 {
+		_, err = rand.Read(rbuf)
+		bufN = 0
+		if err != nil {
+			return pk, err
+		}
+	}
+	pk, err = crypto.ToECDSA(rbuf[bufN : bufN+32])
+	bufN++
+	return pk, err
+}
+
+// flags
 var (
 	path    *string = flag.String("o", "priv.key", "private key file output path")
 	prefix  *string = flag.String("p", "", "prefix (excluding 0x)")
 	longOk  *bool   = flag.Bool("l", false, "accept long prefixes")
+	useFast *bool   = flag.Bool("f", false, "use a potentially faster but less secure function to generate private keys")
 	timeOut *int64  = flag.Int64("t", 0, "maximum acceptable search time in seconds")
 )
 
+// errors
 var (
 	errTooLongInvalid = fmt.Errorf("prefix must be 32 characters or less")
 	errTooLong        = fmt.Errorf("finding a private key for an address with this prefix is likely to take a long time; re-run with the -l flag if you wish to continue")
@@ -87,12 +121,20 @@ func main() {
 		})
 	}
 
+	var k keyFunc
+	if *useFast {
+		k = fastRand
+		rbuf = make([]byte, RBUF_LEN)
+	} else {
+		k = crypto.GenerateKey
+	}
+
 	var (
 		priv *ecdsa.PrivateKey
 		addr common.Address
 	)
 	for ok := false; !ok; ok = strings.HasPrefix(addr.String(), *prefix) {
-		priv, err = crypto.GenerateKey()
+		priv, err = k()
 		if err != nil {
 			log.Fatalln(err)
 		}
