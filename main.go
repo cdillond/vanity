d7e776936448ca59e5e134719777d54919c9314e
cdillond <93200981+cdillond@users.noreply.github.com>
2024-08-01 15:36:49 -0400

    added concurrency

--- main.go
+++ main.go
@@ -3,55 +3,76 @@ package main
 import (
 	"crypto/ecdsa"
 	"crypto/rand"
+	"encoding/hex"
 	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
+type cmpFunc func(common.Address, []byte) bool
+
+func insensitiveCmp(a common.Address, b []byte) bool {
+	hexAddr := hex.AppendEncode(nil, a[:])
+	// panic if hexAddr is shorter than b
+	_ = hexAddr[len(b)-1]
+	for i := 0; i < len(b); i++ {
+		if b[i] != hexAddr[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func sensitiveCmp(a common.Address, b []byte) bool {
+	hexAddr := a.Hex()
+	// panic if hexAddr is shorter than b
+	_ = hexAddr[len(b)-1]
+	for i := 0; i < len(b); i++ {
+		if b[i] != hexAddr[i] {
+			return false
+		}
+	}
+	return true
+}
+
 type keyFunc func() (*ecdsa.PrivateKey, error)
 
-// vars used by fastRand
-var (
-	rbuf []byte
-	bufN int
-)
-
-const RBUF_LEN = 4096
-
-// fastRand reads random data into rbuf and then converts slices of this data into private keys.
+// the func returned by fastRand reads random data into rbuf and then converts slices of this data into private keys.
 // the beginning/end indices of the private key slice are incremented by 1 with each call, so the
 // underlying bytes are reused (until rbuf is exhausted and refilled), but they are interpreted
 // differently by crypto.ToECDSA. In theory, this should greatly reduce the number of syscalls
 // and copies for most prefixes. This would be bad if we were producing multiple private keys,
 // since it could potentially be much easier to guess private keys produced by overlapping data,
 // but, because we are only after 1 key, it is probably fine.
-func fastRand() (pk *ecdsa.PrivateKey, err error) {
-	if bufN == 0 || bufN > RBUF_LEN-32 {
-		_, err = rand.Read(rbuf)
-		bufN = 0
-		if err != nil {
-			return pk, err
+func fastRand(n int, rbuf []byte) func() (pk *ecdsa.PrivateKey, err error) {
+	return func() (pk *ecdsa.PrivateKey, err error) {
+		if n == 0 || n > len(rbuf)-32 {
+			_, err = rand.Read(rbuf)
+			n = 0
+			if err != nil {
+				return pk, err
+			}
 		}
+		pk, err = crypto.ToECDSA(rbuf[n : n+32])
+		n++
+		return pk, err
 	}
-	pk, err = crypto.ToECDSA(rbuf[bufN : bufN+32])
-	bufN++
-	return pk, err
 }
 
 // flags
 var (
-	path    *string = flag.String("o", "priv.key", "private key file output path")
-	prefix  *string = flag.String("p", "", "prefix (excluding 0x)")
-	longOk  *bool   = flag.Bool("l", false, "accept long prefixes")
-	useFast *bool   = flag.Bool("f", false, "use a potentially faster but less secure function to generate private keys")
-	timeOut *int64  = flag.Int64("t", 0, "maximum acceptable search time in seconds")
+	path        *string = flag.String("o", "priv.key", "private key file output path")
+	prefix      *string = flag.String("p", "", "prefix (excluding 0x)")
+	insensitive *bool   = flag.Bool("i", false, "accept case-insensitive solutions")
+	longOk      *bool   = flag.Bool("l", false, "accept long prefixes")
+	useFast     *bool   = flag.Bool("f", false, "use a potentially faster but less secure function to generate private keys")
+	timeOut     *int64  = flag.Int64("t", 0, "maximum acceptable search time in seconds")
 )
 
 // errors
@@ -97,54 +118,68 @@ func main() {
 			log.Fatalln(err)
 		}
 	}
-	*prefix = "0x" + *prefix
+	var (
+		b   []byte
+		cmp cmpFunc
+	)
+	if *insensitive {
+		b = []byte(strings.ToLower(*prefix))
+		cmp = insensitiveCmp
+	} else {
+		b = []byte("0x" + *prefix)
+		cmp = sensitiveCmp
+	}
 
 	log.Println("generating keys. this may take awhile...")
 
-	var (
-		done bool
-		m    sync.Mutex
-	)
+	timedOut := make(<-chan time.Time)
 	if *timeOut > 0 {
-		time.AfterFunc(time.Second*time.Duration(*timeOut), func() {
-			var s string
-			if *timeOut > 1 {
-				s = "s"
-			}
-			// don't want to time out while the writing the key to the filesystem if a key has already been found
-			m.Lock()
-			if !done {
-				log.Fatalln(fmt.Errorf("operation timed out after %d second%s", *timeOut, s))
-			}
-			// basically not needed
-			m.Unlock()
-		})
+		timedOut = time.After(time.Second * time.Duration(*timeOut))
 	}
 
-	var k keyFunc
-	if *useFast {
-		k = fastRand
-		rbuf = make([]byte, RBUF_LEN)
-	} else {
-		k = crypto.GenerateKey
+	ch := make(chan *ecdsa.PrivateKey)
+	for i := 0; i < 16; i++ {
+		go func() {
+			var k keyFunc
+			if !*useFast {
+				k = crypto.GenerateKey
+			} else {
+				var n int
+				if len(*prefix) > 5 {
+					n = 1 << 20 // 1 MiB
+				} else {
+					n = 4 << 10 // 4 KiB
+				}
+				k = fastRand(n, make([]byte, n))
+			}
+			var (
+				pk   *ecdsa.PrivateKey
+				err  error
+				addr common.Address
+			)
+			for ok := false; !ok; ok = cmp(addr, b) {
+				pk, err = k()
+				if err != nil {
+					log.Fatalln(err)
+				}
+				addr = crypto.PubkeyToAddress(pk.PublicKey)
+			}
+			ch <- pk
+		}()
 	}
 
-	var (
-		priv *ecdsa.PrivateKey
-		addr common.Address
-	)
-	for ok := false; !ok; ok = strings.HasPrefix(addr.String(), *prefix) {
-		priv, err = k()
-		if err != nil {
+	select {
+	case priv := <-ch:
+		addr := crypto.PubkeyToAddress(priv.PublicKey)
+		fmt.Println(addr) // print the address first in case the path is /dev/stdout
+		if err = crypto.SaveECDSA(*path, priv); err != nil {
 			log.Fatalln(err)
 		}
-		addr = crypto.PubkeyToAddress(priv.PublicKey)
-	}
-	m.Lock()
-	done = true
-	m.Unlock()
-	fmt.Println(addr)
-	if err = crypto.SaveECDSA(*path, priv); err != nil {
-		log.Fatalln(err)
+	case <-timedOut:
+		var s string
+		if len(*prefix) > 1 {
+			s = "s"
+		}
+		log.Fatalln(fmt.Errorf("operation timed out after %d second%s", *timeOut, s))
 	}
 }
